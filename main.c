198bdef4bde2b83fcd3b092f009da2208ee46185
Nicolas Marzagao <nicolas.marzagao@gmail.com>
2026-05-27 21:49:04 -0300

    feat: better functions

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ size_t write_callback(void* contents, size_t size, size_t nmemb, char** response
     return total_size;
 }
 
-char* get_request_url(const char* path) {
+char* set_request_url(const char* path) {
     char* request_url = malloc(257);
     if (!request_url) return NULL;
     
@@ -25,9 +25,8 @@ char* get_request_url(const char* path) {
     return request_url;
 }
 
-CURLcode make_get_request(CURL* curl, char** response) {
-    printf("[log] GET REQUEST\n");
-    char* request_url = get_request_url("/getUpdates");
+CURLcode make_get_updates(CURL* curl, char** response) {
+    char* request_url = set_request_url("/getUpdates");
 
     curl_easy_setopt(curl, CURLOPT_URL, request_url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
@@ -36,9 +35,11 @@ CURLcode make_get_request(CURL* curl, char** response) {
     return curl_easy_perform(curl);
 }
 
-CURLcode make_post_request(CURL* curl, char** response, const char* body) {
-    printf("[log] POST REQUEST\n");
-    char* request_url = get_request_url("/sendMessage");
+CURLcode send_message(CURL* curl, char** response, long long chat_id, const char* text) {
+    char* request_url = set_request_url("/sendMessage");
+
+    char body[256];
+    sprintf(body, "chat_id=%lld&text=%s\n", chat_id, text);
 
     curl_easy_setopt(curl, CURLOPT_URL, request_url);
     curl_easy_setopt(curl, CURLOPT_POST, 1L);
@@ -47,54 +48,61 @@ CURLcode make_post_request(CURL* curl, char** response, const char* body) {
     return curl_easy_perform(curl);
 }
 
-int main(void) {
+void setup_env_token() {
     char* env_token = getenv("TELEGRAM_TOKEN");
     
     if (env_token == NULL) {
         fprintf(stderr, "Error: TELEGRAM_TOKEN not set\n");
-        return 0;
+        exit(1);
     }
     
     TOKEN = strdup(env_token);
     if (TOKEN == NULL) {
         fprintf(stderr, "Error: Memory allocation failed\n");
-        return 0;
+        exit(1);
+    }
+}
+
+long long get_chat_id(CURL* curl, char** response) {
+    CURLcode res;
+    // add error handling
+    res = make_get_updates(curl, response);
+    if(res == CURLE_OK) {
+        printf("Response:\n%s\n", response[0]);
+    } else {
+        fprintf(stderr, "curl failed: %s\n", curl_easy_strerror(res));
     }
 
-    CURL* curl = curl_easy_init();
+    const char* response_str = response[0];
 
+    if (strstr(response_str, "\"id\"")) {
+        printf("This string contains ID\n");
+    }
+    json_error_t error;
+    json_t* root = json_loads(response_str, 0, &error);
+    json_t* r = json_object_get(root, "result");
+    json_t* head = json_array_get(r, 0);
+    json_t* m = json_object_get(head, "message");
+    json_t* chat = json_object_get(m, "chat");
+    json_t* id = json_object_get(chat, "id");
+    return json_integer_value(id);
+
+}
+
+int main(void) {
+    setup_env_token();
+
+    CURL* curl = curl_easy_init();
     char* response = malloc(1);
     if (!response) return -1;
     response[0] = '\0';
-
     CURLcode res;
-
     if (curl) {
-        res = make_get_request(curl, &response);
-        if(res == CURLE_OK) {
-            printf("Response:\n%s\n", response);
-        } else {
-            fprintf(stderr, "curl failed: %s\n", curl_easy_strerror(res));
-        }
 
-        if (strstr(response, "\"id\"")) {
-            printf("This string contains ID\n");
-        }
+        long long chat_id = get_chat_id(curl, &response);
 
-        json_error_t error;
-        json_t* root = json_loads(response, 0, &error);
-        json_t* r = json_object_get(root, "result");
-        json_t* head = json_array_get(r, 0);
-        json_t* m = json_object_get(head, "message");
-        json_t* chat = json_object_get(m, "chat");
-        json_t* id = json_object_get(chat, "id");
-        long long chat_id = json_integer_value(id);
-
-        response = NULL;
-
-        char body[256];
-        sprintf(body, "chat_id=%lld&text=Hello World!\n", chat_id);
-        res = make_post_request(curl, &response, body);
+        // response = NULL;
+        res = send_message(curl, &response, chat_id, "Hello from another funciton!");
         if(res == CURLE_OK) {
             printf("Response:\n%s\n", response);
         } else {
